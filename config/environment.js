ad945e5dff096f911b90deeed4d5e11b11e4b1ce
Michiel <linearza@users.noreply.github.com>
2015-07-26 15:22:03 +0200

    Fixed content-security console errors. Added roboto font

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
+  var config = {
     modulePrefix: 'generic-company',
     environment: environment,
     baseURL: '/',
@@ -11,6 +11,18 @@ module.exports = function(environment) {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       }
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval' 'unsafe-inline' https://*.firebase.com https://*.firebaseio.com",
+      'font-src': "'self' data: http://fonts.gstatic.com",
+      'frame-src': "https://*.firebase.com https://*.firebaseio.com",
+      'connect-src': "'self' https://api.cloudinary.com res.cloudinary.com https://*.firebase.com https://*.firebaseio.com wss://*.firebaseio.com samewave-client-staging.herokuapp.com samewave-client-demo.herokuapp.com samewave-engine-production.herokuapp.com data:",
+      'img-src': "'self' res.cloudinary.com api.cloudinary.com data:",
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com",
+      'media-src': "'self'",
+
     },
 
     APP: {
@@ -25,23 +37,39 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    // dev CSP settings
+    config.contentSecurityPolicy['script-src'] += " samewave.dev:35729";
+    config.contentSecurityPolicy['connect-src'] += " ws://samewave.dev:35729 http://localhost:3000";
+    config.contentSecurityPolicy['report-uri'] = "http://localhost:4200 http://samewave.dev:4200";
+
+    config.ENGINE_URL = 'http://localhost:3000';
+
+    // config.APP.LOG_RESOLVER = true;
+    config.APP.LOG_ACTIVE_GENERATION = false;
+    config.APP.LOG_TRANSITIONS = true;
+    // config.APP.LOG_TRANSITIONS_INTERNAL = true;
+    config.APP.LOG_VIEW_LOOKUPS = false;
+
+    // Disable features based on the version
+    config.APP.VERSION = 8;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    config.baseURL = '/';
+    config.locationType = 'none';
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    config.APP.LOG_ACTIVE_GENERATION = false;
+    config.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    config.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
 
   }
 
-  return ENV;
+  return config;
 };
